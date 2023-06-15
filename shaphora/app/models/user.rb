c7978a1b56d7fb0afef991d1a7b6e8af8e20b9e8
Shanna Le <shannatle@gmail.com>
2023-06-14 22:54:54 -0700

    Add methods in user model

--- shaphora/app/models/user.rb
+++ shaphora/app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
     validates :password_digest, :first_name, :last_name, presence: true 
     validates :email, presence: true, uniqueness: true, length: { in: 3..255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :password, length: { in: 6..255 }, allow_nil: true
+
+    before_validation :ensure_session_token
+
+    
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.authenticate(password)
+            return user
+        else 
+            nil
+        end 
+    end
+
+    private
+
+    def generate_unique_session_token
+        loop do
+            token = SecureRandom.base64
+            break token unless User.exists?(session_token: token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
 end
