22b95017951e832be4cd0c1a4303fb6bfc5fe9f2
Shanna Le <shannatle@gmail.com>
2023-06-14 23:11:29 -0700

    Add methods for application ctroller

--- shaphora/app/controllers/application_controller.rb
+++ shaphora/app/controllers/application_controller.rb
@@ -2,12 +2,33 @@ class ApplicationController < ActionController::API
 
 # add to transform keys in params before any controller actions, frontend to backend
 # camelCase to snake_case 
-before_action :snake_case_params
+    before_action :snake_case_params
 
-private
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+  
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+  
+    def logout!
+        current_user.reset_session_token! if current_user
+        session[:session_token] = nil
+        @current_user = nil
+    end
+  
+    def require_logged_in
+        unless current_user
+            render json: { message: 'Unauthorized' }, status: :unauthorized 
+        end
+    end
 
-def snake_case_params
-  params.deep_transform_keys!(&:underscore)
-end
+    private
+
+    def snake_case_params
+        params.deep_transform_keys!(&:underscore)
+    end
+
 
 end
