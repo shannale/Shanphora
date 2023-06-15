5b295dc58b1098970a9589cd3e789590d088a96b
Shanna Le <shannatle@gmail.com>
2023-06-14 23:32:28 -0700

    Complete api users controller

--- shaphora/app/controllers/api/users_controller.rb
+++ shaphora/app/controllers/api/users_controller.rb
@@ -1,4 +1,22 @@
 class Api::UsersController < ApplicationController
-  def create
-  end
+
+    wrap_parameters include: User.attribute_names + ['firstName', 'lastName', 'password']
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :first_name, :last_name, :password)
+    end
 end
