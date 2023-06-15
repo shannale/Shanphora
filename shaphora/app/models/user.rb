7d0e2424292b7808684766dea1f99292d80553bf
Shanna Le <shannatle@gmail.com>
2023-06-14 22:45:01 -0700

    Add user validations

--- shaphora/app/models/user.rb
+++ shaphora/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :session_token, presence: true, uniqueness: true 
+    validates :password_digest, :first_name, :last_name, presence: true 
+    validates :email, presence: true, uniqueness: true, length: { in: 3..255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, length: { in: 6..255 }, allow_nil: true
 end
