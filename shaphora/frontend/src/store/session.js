2a77caa9fce1bbc8cc7ae580e8a23d75801e225e
Shanna Le <shannatle@gmail.com>
2023-06-16 15:15:16 -0700

    Add session and LoginForm

--- shaphora/frontend/src/store/session.js
+++ shaphora/frontend/src/store/session.js
@@ -1,11 +1,61 @@
 import csrfFetch from "./csrf";
+
+
+
+const SET_CURRENT_USER = 'session/setCurrentUser';
+const REMOVE_CURRENT_USER = 'session/removeCurrentUser';
+
+const setCurrentUser = (user) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: user
+  };
+};
+
+const removeCurrentUser = () => {
+  return {
+    type: REMOVE_CURRENT_USER
+  };
+};
+
+export const login = (user) => async (dispatch) => {
+  const { credential, password } = user;
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({
+      credential,
+      password
+    })
+  });
+  const data = await response.json();
+  dispatch(setCurrentUser(data.user));
+  return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return { ...state, user: action.payload };
+    case REMOVE_CURRENT_USER:
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
+
 export function storeCSRFToken(response) {
     const csrfToken = response.headers.get("X-CSRF-Token");
     if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
-  }
+}
   
   export async function restoreCSRF() {
     const response = await csrfFetch("/api/session");
     storeCSRFToken(response);
     return response;
-  }
+}
+
+export default sessionReducer;
+
+  
