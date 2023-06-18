8957d7c78fda053a2f932a2169b11d969727e459
Shanna Le <shannatle@gmail.com>
2023-06-18 10:23:51 -0700

    Fix nav bar alignment

--- shaphora/frontend/src/components/Navigation/ProfileButton.js
+++ shaphora/frontend/src/components/Navigation/ProfileButton.js
@@ -1,55 +1,48 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
-import SignupFormModal from "../SignupFormModal";
-import LoginFormModal from "../LoginFormModal";
-import { Modal } from '../../context/Modal';
 
 function ProfileButton({ user }) {
-  const dispatch = useDispatch();
-  const [showMenu, setShowMenu] = useState(false);
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    
+    const openMenu = () => {
+      if (showMenu) return;
+      setShowMenu(true);
+    };
+    
+    useEffect(() => {
+      if (!showMenu) return;
 
+      const closeMenu = () => {
+        setShowMenu(false);
+      };
 
-  const sessionUser = useSelector(state => state.session.user);
-  
-  const openMenu = () => {
-    if (showMenu) return;
-    setShowMenu(true);
-  };
+      document.addEventListener('click', closeMenu);
+    
+      return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
 
-  useEffect(() => {
-    if (!showMenu) return;
-
-    const closeMenu = () => {
-      setShowMenu(false);
+    const logout = (e) => {
+      e.preventDefault();
+      dispatch(sessionActions.logout());
     };
 
-    document.addEventListener('click', closeMenu);
-  
-    return () => document.removeEventListener("click", closeMenu);
-  }, [showMenu]);
-
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
-
-  return (
-    <>
-      <button onClick={openMenu}>
-        <i className="fa-solid fa-user-circle" />
-      </button>
-      {showMenu && (
-        <ul className="profile-dropdown">
-          <li>{user.firstName}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul>
-      )}
-    </>
-  );
+    return (
+      <>
+        <button onClick={openMenu}>
+          <i className="fa-solid fa-user-circle" />
+        </button>
+        {showMenu && (
+          <ul className="profile-dropdown">
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </ul>
+        )}
+      </>
+    );
 }
 
 export default ProfileButton;
-
--- shaphora/frontend/src/components/Navigation/index.js
+++ shaphora/frontend/src/components/Navigation/index.js
@@ -7,46 +7,47 @@ import SignupFormModal from '../SignupFormModal';
 import SearchBar from '../SearchBar';
 import './Navigation.css';
 
-function Navigation(){
+function Navigation() {
   const sessionUser = useSelector(state => state.session.user);
 
   let sessionLinks;
   if (sessionUser) {
-    sessionLinks = (
-      <ProfileButton user={sessionUser} />
-    );
-  } 
+    sessionLinks = <ProfileButton user={sessionUser} />;
+  }
 
   return (
     <>
-        <div className="nav-bar"> 
-            <nav> 
-                  <div className="top-line"> 
-                      <span className="top-line-text"> 
-                          Get FREE shipping on all orders when you join Beauty Insider. Exclusions/terms apply.† &nbsp;
-                          <strong className="bold-words">LEARN MORE</strong>
-                      </span>
-                  </div>
+      <div className="nav-bar">
+        <nav>
+            <div className="nav-box"> 
+              <div className="top-line">
+                    <span className="top-line-text">
+                      Get FREE shipping on all orders when you join Beauty Insider. Exclusions/terms apply.† &nbsp;
+                      <strong className="bold-words">LEARN MORE</strong>
+                    </span>
+              </div>
 
-                  <div className="home-link"> 
-                      <NavLink className="home-title" exact={true} to="/">S H A P H O R A</NavLink>
+              <div className="home-link">
+                    <NavLink className="home-title" exact={true} to="/">
+                      S H A P H O R A
+                    </NavLink>
+                    <div className="home-links-container">
                       {sessionLinks}
-                  </div> 
+                          <div className="search-bar">
+                            <SearchBar />
+                          </div>
 
-                  <div className='search-bar'> <SearchBar /> </div>
+                          <div className="signin">
+                            <LoginFormModal />
+                            <SignupFormModal />
+                          </div>
+                    </div>
 
-                  <div className='signin'> <LoginFormModal /> </div>
-
-                  <div className='signup'> <SignupFormModal /> </div>
-            </nav>
-
-        </div>
-
-
+            </div>
+          </div>
+        </nav>
+      </div>
     </>
-  
-
-
   );
 }
 
