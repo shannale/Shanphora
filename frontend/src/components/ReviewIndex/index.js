90a4b9c7f9d9331bae067ccf8a289150497c307f
Shanna Le <shannatle@gmail.com>
2023-06-26 02:14:28 -0700

    Fix current user for product show page

--- frontend/src/components/ReviewIndex/index.js
+++ frontend/src/components/ReviewIndex/index.js
@@ -1,15 +1,19 @@
 import ReviewShow from "../ReviewShow"
 import React from "react";
 import ReviewForm from "../ReviewForm";
+import { useSelector } from "react-redux";
+
 
 const ReviewIndex = ({ reviews }) => {
+    
+const currentUser = useSelector(state => state.session.user);
 
   return (
     <div className="review-container">
       {Object.values(reviews).map((review) => (
         <ReviewShow key={review.id} review={review} />
       ))}
-      <ReviewForm />
+      {currentUser ? <ReviewForm /> : (<div className="review-form-message"> Please Sign In To Leave A Review </div>)}
     </div>
   );
 };
