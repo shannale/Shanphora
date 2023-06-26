ab5f209ab9bb0801986155ea8bad5147572e9a5c
Shanna Le <shannatle@gmail.com>
2023-06-26 15:05:22 -0700

    Profile hover

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -2,15 +2,26 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import profile from "./Images/user.png"
+import beforeProfile from "./Images/before-hover.png"
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
     
     const openMenu = () => {
       if (showMenu) return;
       setShowMenu(true);
     };
+
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
+    
     
     useEffect(() => {
       if (!showMenu) return;
@@ -31,9 +42,12 @@ function ProfileButton({ user }) {
 
     return (
       <>
-        <button onClick={openMenu} style={{ border: "none", outline: "none", boxShadow: "none" }} className="button-box">
-        <img className='user-profile-img' src={profile} alt='' />
-        </button>
+      <div className="profile-button-box-picture">
+          <button onClick={openMenu} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{ border: "none", outline: "none", boxShadow: "none" }} className="button-box">
+            <img className='user-profile-img' src={isHovered ? profile : beforeProfile} alt='user image' />
+          </button>
+
+      </div>
 
         <div className={!showMenu ? "hidden" : null}> 
             <div className="profile-dropdown logout-dropdown">
@@ -53,3 +67,5 @@ function ProfileButton({ user }) {
 }
 
 export default ProfileButton;
+
+
