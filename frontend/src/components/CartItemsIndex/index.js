3dc8f2d128d001b207956e4c62c8253794634d93
Shanna Le <shannatle@gmail.com>
2023-06-26 22:56:49 -0700

    Reposition shopping cart button

--- frontend/src/components/CartItemsIndex/index.js
+++ frontend/src/components/CartItemsIndex/index.js
@@ -1,12 +1,6 @@
 import { Redirect } from "react-router-dom";
 
 const CartItems = () => { 
-	return (
-		<div>
-            <button style={{background: 'none', border: 'none', padding: 0, cursor: 'pointer', appearance: 'none', outline: 'none'}} onClick={handleClick}>
-                <Redirect to='/' />
-            </button>
-		</div>
-	);
+
 };
 export default CartItems;
